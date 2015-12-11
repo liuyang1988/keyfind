3b84b23a5c38976728f3c1ca2bb412273f9422bf
liuyang1988 <liuyang1988cq@126.com>
2015-12-11 17:32:06 +0800

    增加读取多个listwidget items
    filenames = self.listWidget_filenames.selectedIndexes()
    for f in filenames:
    print("i am here")
    print(f.data())

--- keyfind.py
+++ keyfind.py
@@ -41,18 +41,7 @@ class KeyFind(QDialog, Ui_Dialog):
         #将选中的文件名加入listWidget中
         self.listWidget_filenames.addItems(files)
         
-     #后续这段代码并没有什么用，需要删除                        
-    @pyqtSlot(QModelIndex)
-    def on_listWidget_filenames_clicked(self, index):
-        """
-        Slot documentation goes here.
-        """
-        # TODO: not implemented yet
-        #raise NotImplementedError
-        print ("hello")
-        row = self.listWidget_filenames.currentRow()
-        currentfile = self.listWidget_filenames.item(row).text()
-        print (currentfile)   
+     
         
      # 对选择的文件进行关键字查找，关键字在程序中定义好。12092015
     @pyqtSlot()
@@ -95,29 +84,34 @@ class KeyFind(QDialog, Ui_Dialog):
         # TODO: not implemented yet
         #raise NotImplementedError
         #pp 正则表达式，或许可以移到init中
-        import re
-        keyword1= re.compile('ddd[a-e] \d{4}')
-        keyword2 = re.compile('\.\S{6}[AS]')
+#        import re
+#        keyword1= re.compile('ddd[a-e] \d{4}')
+#        keyword2 = re.compile('\.\S{6}[AS]')
         
-        filenames = self.listWidget_filenames.currentItem() 
-        row = self.listWidget_filenames.currentRow()
-        currentfile = self.listWidget_filenames.item(row).text()
-        print (currentfile)
-        ###
-        print (filenames)
-        for f  in open(filenames, 'r'):
-            for line in f:
-                rs1 = keyword1.findall(line)
-                if rs1:
-                  print(rs1)
-                self.listWidget_sr_show.addItems(rs1)
-                
-                
-                rs2 = keyword2.findall(line)
-                if rs2:
-                   print(rs2)
-                   self.listWidget_sr_show.addItems(rs2) 
-        ###
+        filenames = self.listWidget_filenames.selectedIndexes()
+        for f in filenames:
+            print("i am here")
+            print(f.data())
+            
+ 
+#        row = self.listWidget_filenames.currentRow()
+#        currentfile = self.listWidget_filenames.item(row).text()
+#        print (filenames)
+        
+#        print (filenames)
+#        for f  in open(filenames, 'r'):
+#            for line in f:
+#                rs1 = keyword1.findall(line)
+#                if rs1:
+#                  print(rs1)
+#                self.listWidget_sr_show.addItems(rs1)
+#                
+#                
+#                rs2 = keyword2.findall(line)
+#                if rs2:
+#                   print(rs2)
+#                   self.listWidget_sr_show.addItems(rs2) 
+        
     
     @pyqtSlot()
     def on_pushButton_openselectfile_clicked(self):
